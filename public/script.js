30cf81bcda8499d47cc43a520513dafac33d265e
frenki <frenkiqose@gmail.com>
2025-03-01 20:18:05 +0100

    base movement is ready, works between different windows

--- public/script.js
+++ public/script.js
@@ -9,10 +9,23 @@ function draw_component(players){
     draw.clearRect(0, 0, WIDTH, HEIGHT);
 
     if (!players) {console.log('players is empty');return};
+
     players.forEach(player => {
-        console.log(player);
-        draw.fillRect(Object.values(player)[0].x, Object.values(player)[0].y, Object.values(player)[0].width, Object.values(player)[0].height);
+        let tank = Object.values(player)[0];
+        draw.save(); //save state
+        //i have to translate the object i want to draw to the origin and then back
+        draw.translate(tank.x + tank.height/2, tank.y + tank.width/2);
+        draw.rotate(tank.angle); //rotate canvas
+        draw.fillRect(-tank.height/2, -tank.width/2, tank.height, tank.width); //draw object
+        draw.fillRect(-tank.height/2, -tank.width/2 + tank.width * 0.35, tank.height*1.3, tank.width * 0.3);
+        draw.restore(); //restore to previous
     });
+}
+let direction = {
+    left: false,
+    right: false,
+    forwards: false,
+    backwards: false
 }
 
 const socket = io({
@@ -20,6 +33,7 @@ const socket = io({
         clientId: localStorage.getItem("clientId") || generateClientId()
     }
 });
+update();
 
 function generateClientId() {
     const id = Math.random().toString(36).substring(2, 9);
@@ -30,12 +44,42 @@ function generateClientId() {
 socket.on("connect", () => {
     console.log('connected successfully');
 });
-
 socket.on('new_connection', (players)=>{
     draw_component(players);
 })
-
-
+socket.on('update', (players)=>{
+    draw_component(players);
+})
 socket.on("disconnect", () => {
-    document.body.innerHTML = "Disconnected.";
+    console.log('disconnected');
 });
+
+window.addEventListener('keydown', (event)=>{
+    if (event.key == 'w' || event.key == 'W') {
+        direction.forwards = true;
+        direction.backwards = false;
+    }
+    if (event.key == 's' || event.key == 'S') {
+        direction.backwards = true;
+        direction.forwards = false;
+    }
+    if (event.key == 'a' || event.key == 'A') {
+        direction.left = true;
+        direction.right = false;
+    }
+    if (event.key == 'd' || event.key == 'D') {
+        direction.right = true;
+        direction.left = false;
+    }
+});
+window.addEventListener('keyup', (event)=>{
+    if (event.key == 'w' || event.key == 'W') direction.forwards = false;    
+    if (event.key == 's' || event.key == 'S') direction.backwards = false;
+    if (event.key == 'a' || event.key == 'A') direction.left = false;
+    if (event.key == 'd' || event.key == 'D') direction.right = false;
+});
+
+setInterval(update, 1000/60);
+function update(){
+    socket.emit('movement', direction);
+}
--- server/server.js
+++ server/server.js
@@ -16,17 +16,38 @@ class tank{
     y;
     h_speed;
     v_speed;
+    r_speed;
+    angle;
+    a;
     constructor(){
-        this.height = 50;
-        this.width = 25;
-        this.x = Math.round(1000 * Math.random());
+        this.height = 40;
+        this.width = 35;
+        this.x = Math.round(200 * Math.random());
         this.y = 100;
-        this.h_speed = 1;
-        this.v_speed = 1;
+        this.h_speed = 0;
+        this.v_speed = 0;
+        this.angle = 0;
+        this.a = 3;
+        this.r_speed = 0.1;
+    }
+
+    move(direction){
+        if (direction.left) this.angle -= this.r_speed;
+        if (direction.right) this.angle += this.r_speed;
+        if (direction.forwards){ this.h_speed = Math.cos(this.angle) * this.a; this.v_speed = Math.sin(this.angle) * this.a;}
+        else if (direction.backwards){ this.h_speed = -Math.cos(this.angle) * this.a; this.v_speed = -Math.sin(this.angle) * this.a;}
+        else{this.h_speed = 0 ; this.v_speed = 0;}
+
+        this.x += this.h_speed;
+        this.y += this.v_speed;
     }
 }
 let players = [];
 let ids = [];
+
+let last_time = Date.now();
+const FIXED_TIME = 1000/60;
+
 io.on("connection", (socket) => {
     let clientId = socket.handshake.query.clientId;
     
@@ -41,11 +62,27 @@ io.on("connection", (socket) => {
 
     io.emit('new_connection', players);
     socket.on('movement', (direction)=>{
-        console.log(direction);
-        //TODO : update the players array here
+        let recent_time = Date.now();
+        let elapsed = recent_time - last_time;
+
+        let found_object = players.find(obj => obj.hasOwnProperty(clientId));
+        let tank = found_object[clientId];
+
+        if (elapsed > FIXED_TIME) {
+            tank.move(direction);
+            io.emit('update', players);
+            last_time = recent_time;
+        }
+        
+    });
+
+    socket.on('disconnect',()=>{
+        let found_object = players.find(obj => obj.hasOwnProperty(clientId));
+        players.splice(players.indexOf(found_object), 1);
+        console.log(clientId, 'has disconnected');
+        io.emit('update', players);
     })
 });
-
 
 server.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
